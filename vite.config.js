7fbf00beb31a4e5c365270a1210061c26e58f3e8
illysito <illyalukyanovdesign@gmail.com>
2026-02-21 12:15:59 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import eslintPlugin from 'vite-plugin-eslint'
 
 // vite.config.js
 export default defineConfig({
-  base: '/source/',
+  base: '/',
   plugins: [eslintPlugin({ cache: false })],
   server: {
     host: 'localhost',
